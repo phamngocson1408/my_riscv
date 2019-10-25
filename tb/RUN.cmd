81dfb555c93fa1ad362923d08f1c7fb3b351a503
pnson <phamngocson1408@gmail.com>
2019-10-25 16:07:13 +0900

    Add ass testbench

--- tb/RUN.cmd
+++ tb/RUN.cmd
@@ -1,6 +1,6 @@
 	testbench.v
-	../core/rv32im/*.v
+	../core/rv32imac/*.v
 
-	+incdir+../core/rv32im
+	+incdir+../core/rv32imac
 	-cpp g++-4.8 -cc gcc-4.8
 	-LDFLAGS -Wl,-no-as-needed
--- tb/test_vector/ass_code/simple/main.s
+++ tb/test_vector/ass_code/simple/main.s
@@ -75,14 +75,47 @@
 # reg + reg -----------------
 .global main
 main:
- li	ra,0x1		# load 0x1 to x1
- li	sp,0x3		# load 0x2 to x2
+# li	ra,0x1		# load 0x1 to x1
+# li	sp,0x3		# load 0x2 to x2
 # add	gp,ra,sp	# x3 = x1 + x2
 # sub	gp,ra,sp	# x3 = x1 - x2
 # sll	gp,ra,4		# x2 = x1 << 4	
 # srl	gp,ra,4		# x2 = x1 >> 4	
- slt	gp,ra,sp	# x3 = x1 < x2
- sltu	gp,ra,sp	# x3 = x1 < x2
- xor	gp,ra,sp	# x3 = x1 xor x2
- or	gp,ra,sp	# x3 = x1 or x2
- and	gp,ra,sp	# x3 = x1 and x2
+# slt	gp,ra,sp	# x3 = x1 < x2
+# sltu	gp,ra,sp	# x3 = x1 < x2
+# xor	gp,ra,sp	# x3 = x1 xor x2
+# or	gp,ra,sp	# x3 = x1 or x2
+# and	gp,ra,sp	# x3 = x1 and x2
+# LWSP -----------------
+# li	sp,1 		#load 1 to x1
+# lw	a0,2(sp)	#x2 = *(x2 + 2)
+# lw	a0,2(a1)	#x2 = *(x2 + 2)
+ addi	a1, a1, 20	#caddi
+ addi	a1, a1, 20	#caddi
+ addi	x2, x2, 2	#caddi16sp
+ addi	x1, x2, 11	#caddi4spn
+ slli	a1, a1, 2	#cslli
+ srli	a1, a1, 2	#csrli
+ srai	a1, a1, 1	#csrai
+ andi	a1, a1, 4	#candi
+ add	a0, x0, a1	#cmv
+ add	a0, a0, a1	#cadd
+# and	a0, a0, a1	#cand
+# or	a0, a0, a1	#cor
+# xor	a0, a0, a1	#cxor
+ sub	a0, a0, a1	#csub
+ addi	x0, x0, 0	#cnop
+ sub	a0, a0, a1	#csub
+ addi	a0, x0, 4	#cli
+ lui	a1, 4		#clui
+ lw 	a1,4(x2)	#csplw
+ sw 	a0,4(x2)	#cswsp
+ lw 	a1,4(x2)	#csplw
+ lw 	a1,4(x8)	#clw
+ sw 	a0,4(x8)	#csw
+ lw 	a1,4(x8)	#clw
+ jal 	x0,0xa		#cj
+ jal 	x1,0xa		#cjal
+ jalr 	x0, a0, 0	#cjr
+ beq 	a0, x0, 0x8	#cbeqz
+ bne 	a0, x0, 0x8	#cbnez
